db400c26a53049c1def9d00a72cdf01a7e4ca1d4
tjcardinal <tylerjcardinal@gmail.com>
2021-05-31 16:03:36 -0500

    Add database unit test cases

--- tests/test_database.py
+++ tests/test_database.py
@@ -1,1 +1,43 @@
-from mtgdb import database
+import mtgdb.database
+
+def test_db_creation():
+    pass
+
+def test_adding_one_new_card():
+    pass
+
+def test_adding_many_new_card():
+    pass
+
+def test_adding_one_old_card():
+    pass
+
+def test_adding_many_old_card():
+    pass
+
+def test_removing_under_card_count():
+    pass
+
+def test_removing_exactly_card_count():
+    pass
+
+def test_removing_over_card_count():
+    pass
+
+def test_removing_nonexistent_card():
+    pass
+
+def test_updating_card():
+    pass
+
+def test_updating_nonexistent_card():
+    pass
+
+def test_searching_card():
+    pass
+
+def test_searching_nonexistent_card():
+    pass
+
+def test_make_csv():
+    pass
